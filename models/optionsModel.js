fe13a38a232f76178ce3edf1037a0c055ff8b35b
djg07 <djg07>
2015-04-19 20:31:10 -0700

    Model + view logic update

--- models/optionsModel.js
+++ models/optionsModel.js
@@ -10,11 +10,30 @@ app.optionsModel = Backbone.Model.extend({
     defaults: {
 
         size: "",
-        topLeft: "",
-        topRight: "",
-        bottomLeft: "",
-        bottomRight: "",
+        topLeft: {
+            value: "",
+            link: $("#topLeftPreview")
+        },
+        topRight: {
+            value: "",
+            link: $("#topRightPreview")
+        },
+        bottomLeft: {
+            value: "",
+            link: $("#bottomLeftPreview")
+
+        },
+        bottomRight: {
+            value: "",
+            link: $("#bottomRightPreview")
+        }
 
 
+    },
+
+    changeValue: function(e) {
+        console.log($(e.currentTarget).val())
+        console.log(e);
+        this.set("topLeft.value", "FSFDS");
     }
 });
--- views/optionsView.js
+++ views/optionsView.js
@@ -7,15 +7,31 @@ var app = app || {};
 app.optionsView = Backbone.View.extend({
     el: "#optionsContainer",
     template: _.template($("#optionsTemplate").html()),
-    initialize: function() {
+    initialize: function(options) {
         this.render();
         this.model.on("change", this.render, this)
+
+    },
+
+    events: {
+        "change input.gridOption": "contentChanged"
+    },
+
+    contentChanged: function(e) {
+        alert('content changed');
+        //console.log(e);
+        //debugger;
+        this.model.changeValue(e);
+
+        //console.log(this.model);
+
+        //$('.hexOptionsPreview')
     },
 
     render: function() {
         console.log("rendering")
         this.$el.html(this.template());
-        console.log(this.$el)
+
     }
 
 
